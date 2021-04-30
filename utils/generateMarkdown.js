76901ecf68d002db06e5b0e31546d56b241a246f
Vilma Qerama <vilma.qerama@hotmail.com>
2021-04-30 19:43:29 +0100

    suggested file structure

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,20 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const renderLicenseBadge = (license) => {};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+const renderLicenseLink = (license) => {};
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+const renderLicenseSection = (license) => {};
+
+// TODO: Create a function to generate markdown for README
+const generateMarkdown = (data) => {
+  return `# ${data.title}
+
+`;
+};
+
+module.exports = generateMarkdown;
