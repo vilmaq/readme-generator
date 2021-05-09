95ca6651641b9638186ee18fa533f3040362312a
Vilma Qerama <vilma.qerama@hotmail.com>
2021-05-09 23:24:12 +0100

    add renderLicenseLink function

--- output/README.md
+++ output/README.md
@@ -1,51 +1,57 @@
+# test
 
-  # Test
-  Test
-  ![GPL_3.0 license](https://img.shields.io/badge/license-MIT-green) 
-  
-  ## Description of Application
-  2
-  
-  ## Table of Contents
-  - [Application Title](#application-title)
-    - [Description](#description-of-application)
-    - [Installation Instructions](#installation-instructions)
-    - [Badge](#badge)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-  
-  ##Badge
-  1
+test
+![GPL_3.0 license](https://img.shields.io/badge/license-MIT-green)
 
-  ##License 
-  GPL_3.0
+## Description of Application
 
+test
 
-  ## Installation Instructions
-  2
+## Table of Contents
 
+- [Application Title](#application-title)
+  - [Description](#description-of-application)
+  - [Installation Instructions](#installation-instructions)
+  - [Badge](#badge)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
-  ##Usage
-  3
+## Badge
 
+    ![GPL_3.0 badge](https://img.shields.io/badge/license-GPL_3.0-green)
 
-  
-  ## Contributing
-  4
-  
-  ## Tests
-  npm install, npm inquirer
-  
-  ## Screenshots 
-  test.com
+## License
 
+This project is licensed under the [GPL_3.0](https://www.gnu.org/licenses/gpl-3.0.en.html) license.
 
-  ## Github 
-  - [Github Project Repository](test) 
-  - [Github Project URL](test.com) 
+## Installation Instructions
 
-  ## Contact
-  If you have any questions about the repo, open an issue or contact me directly at vq@test.com. 
+test
+
+## Usage
+
+test
+
+## Contributing
+
+test
+
+## Tests
+
+undefined
+
+## Screenshots
+
+test.com
+
+## Github
+
+- [Github Project Repository](test.com)
+- [Github Project URL](test.com)
+
+## Contact
+
+If you have any questions about the repo, open an issue or contact me directly at vilma.qerama@test.com.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,46 @@
 const inquirer = require("inquirer");
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = (license) => {};
+const renderLicenseBadge = (license) => {
+  // Create a function that returns the license link
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-const renderLicenseLink = (license) => {};
+  if (license !== "None") {
+    return `
+  https://img.shields.io/badge/license-${license}-green
+`;
+    // If there is no license, return an empty string
+  } else {
+    return "";
+  }
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-const renderLicenseSection = (license) => {};
+const renderLicenseLink = (license) => {
+  // Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "APACHE_2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL_3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0.en.html";
+    case "BSD_3":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    case "None":
+      return "";
 
+    default:
+      return "";
+  }
+};
+const renderLicenseSection = (license) => {
+  const licenseLink = renderLicenseLink(license);
+  if (license !== "None") {
+    return `
+  This project is licensed under the  [${license}](${licenseLink}) license.
+  `;
+  }
+};
 // TODO: Create a function to generate markdown for README
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (answers) => {
@@ -18,7 +48,6 @@ const generateMarkdown = (answers) => {
   const {
     applicationTitle,
     applicationDescription,
-    applicationBadge,
     applicationLicense,
     installationInstructions,
     usageInfo,
@@ -29,12 +58,16 @@ const generateMarkdown = (answers) => {
     gitURL,
     email,
   } = answers;
+
+  const applicationBadge = renderLicenseBadge(applicationLicense);
+  const licenseSection = renderLicenseSection(applicationLicense);
+
   return `
   # ${applicationTitle}
-  ${applicationTitle}
+  ${applicationTitle} ![${applicationLicense} license](${applicationBadge})
   ![${applicationLicense} license](https://img.shields.io/badge/license-MIT-green) 
   
-  ## Description of Application
+  ## Description
   ${applicationDescription}
   
   ## Table of Contents
@@ -48,11 +81,12 @@ const generateMarkdown = (answers) => {
     - [Tests](#tests)
     - [Questions](#questions)
   
-  ##Badge
-  ${applicationBadge}
 
-  ##License 
-  ${applicationLicense}
+
+
+  ##License
+
+  ${licenseSection}
 
 
   ## Installation Instructions
@@ -62,13 +96,14 @@ const generateMarkdown = (answers) => {
   ##Usage
   ${usageInfo}
 
-
   
   ## Contributing
   ${contributingGuidelines}
+
   
   ## Tests
   ${testInformation}
+
   
   ## Screenshots 
   ${screenshot}
@@ -77,6 +112,7 @@ const generateMarkdown = (answers) => {
   ## Github 
   - [Github Project Repository](${gitRepo}) 
   - [Github Project URL](${gitURL}) 
+  
 
   ## Contact
   If you have any questions about the repo, open an issue or contact me directly at ${email}. `;
